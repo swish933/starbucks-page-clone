291fa4e8cb5fc5438d51328ee8334c10cf61b044
Ikem Nomso <ikemnomso@gmail.com>
2021-06-17 13:03:24 +0100

    mobile nav

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const toggleMenu = () => {
+	let nav = document.querySelector('#primary-nav-panel');
+	let navSecondary = document.querySelector('#secondary-nav-panel');
+	nav.classList.toggle('block');
+	if (!navSecondary.classList.contains('block')) {
+		return;
+	}
+	nav.classList.remove('block');
+	navSecondary.classList.remove('block');
+};
+
+const openSubmenu = () => {
+	let navPrimary = document.querySelector('#primary-nav-panel');
+	let navSecondary = document.querySelector('#secondary-nav-panel');
+	navSecondary.classList.add('block');
+};
+
+const closeSubmenu = () => {
+	let navPrimary = document.querySelector('#primary-nav-panel');
+	let navSecondary = document.querySelector('#secondary-nav-panel');
+
+	navSecondary.classList.remove('block');
+	navPrimary.classList.add('block');
+};
