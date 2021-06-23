3990f807691960803d731874ab898d7fd137cbf5
Ikem Nomso <ikemnomso@gmail.com>
2021-06-23 19:24:52 +0100

    Added interactive tabbed section

--- js/index.js
+++ js/index.js
@@ -22,3 +22,24 @@ const closeSubmenu = () => {
 	navSecondary.classList.remove('block');
 	navPrimary.classList.add('block');
 };
+
+const openPanel = (evt, panelName) => {
+	let i, tabcontent, tabslider;
+
+	//hide all tabPanels
+	tabcontent = document.getElementsByClassName('tab-content');
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none';
+	}
+	//remove active class from
+	tabslider = document.getElementsByClassName('tab-slider');
+	for (i = 0; i < tabslider.length; i++) {
+		tabslider[i].classList.remove('tab-slider--active');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(panelName).style.display = 'flex';
+	evt.currentTarget.lastElementChild.classList.add('tab-slider--active');
+};
+
+window.onload = document.getElementById('tabLink0').click();
